e591e0ed60aa199c253995cbcc526c157b61e967
Szemin <majkeloos2137@gmail.com>
2024-04-18 13:08:08 +0200

    Connection string and EF added

---
+++ Workshop.Domain/Entities/Workshop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.Domain.Entities
+{
+    public class Workshop
+    {
+        public required int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public WorkshopContactDetails ContactDetails { get; set; } = default!;
+
+        public string EncodedName { get; private set; } = default!;
+
+        public void EncodeName() => EncodedName.ToLower().Replace(" ", "-");
+    }
+}
---
+++ Workshop.Domain/Entities/WorkshopContactDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.Domain.Entities
+{
+    public class WorkshopContactDetails
+    {
+        public string?  PhoneNumber { get; set; }
+        public string? Mail { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+    }
+}
